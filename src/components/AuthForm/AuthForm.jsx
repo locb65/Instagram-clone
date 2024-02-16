fa88d61840ecf3f12a4dcd3de14de92515e4af39
locb65 <locb65@gmail.com>
2024-02-15 23:13:57 -0500

    Simple AuthFrom set up

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { Box, Button, Image, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 export const AuthForm = () => {
+    const [isLogin, setIsLogin] = useState(true)
   return (
-    <div>AuthForm</div>
+    <>
+    <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+    <VStack spacing={4}>
+        <Image src='logo.png' h={24} cursor={"pointer"} alt='Instagram'/>
+        <Input
+            placeholder='Email'
+            fontSize={14}
+            type='email'
+         />
+        <Input
+            placeholder='Password'
+            fontSize={14}
+            type='password'
+         />
+
+         {!isLogin ? (
+            <Input
+                placeholder='Confirm Password'
+                fontSize={14}
+                type='password'
+            />
+         ) : null}
+
+         <Button w={"full"} colorScheme='blue' size={'sm'} fontSize={14}>
+            {isLogin ? "Log In" : "Sign Up"}
+         </Button>
+    </VStack>
+    </Box>
+    </>
   )
 }
