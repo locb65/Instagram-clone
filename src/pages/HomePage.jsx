74d6e959c21db7ad0f7068eb7113fdf89bc027b2
locb65 <locb65@gmail.com>
2024-02-22 18:02:25 -0500

    basic home screen view setup complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,21 @@
+import { Box, Container, Flex } from '@chakra-ui/react'
 import React from 'react'
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+   <Container maxW={"container.lg"}>
+    <Flex gap={20}>
+      {/* left side of home content */}
+      <Box>
+        Feed Box
+      </Box>
+      {/* Right side of Home content */}
+      <Box>
+        Suggested
+      </Box>
+
+    </Flex>
+
+   </Container>
   )
 }
