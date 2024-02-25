eb30021326c19cffb49735b331188d754754b441
locb65 <locb65@gmail.com>
2024-02-25 14:04:53 -0500

    added suggested user content

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,15 +1,31 @@
-import { VStack } from "@chakra-ui/react"
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react"
 import { SuggestedHeader } from "./SuggestedHeader"
 import { SuggestedUser } from "./SuggestedUser"
+import { Link as RouterLink } from "react-router-dom"
 
 export const SuggestedUsers = () => {
   return (
     <VStack py={8} px={6} gap={4}>
         <SuggestedHeader />
-        <SuggestedUser />
-        <SuggestedUser />
-        <SuggestedUser />
-        <SuggestedUser />
+        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+            <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+                Suggested for you
+            </Text>
+            <Text fontSize={12} fontWeight={"bold"} _hover={{color: "gray.400"}} cursor={"pointer"}>
+                See All
+            </Text>
+
+        </Flex>
+        <SuggestedUser name="Dan Abrahmov" followers={1392} avatar="https://bit.ly/dan-abramov"/>
+        <SuggestedUser name="Ryan Florence" followers={567} avatar="https://bit.ly/ryan-florence" />
+        <SuggestedUser name="Christian Nwamba" followers={759} avatar="https://bit.ly/code-beast"/>
+
+        <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+        © 2024 Built by {" "}
+        <Link href="https://www.github.com/locb65" target="_blank" color={"blue.500"} fontSize={14}>
+            locb65
+        </Link>
+        </Box>
     </VStack>
   )
 }
