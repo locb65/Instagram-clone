1373af2ea1a980de58ac8376c304db44aec468e4
locb65 <locb65@gmail.com>
2024-02-25 13:17:02 -0500

    loading state functional

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -1,4 +1,4 @@
-import { Container } from '@chakra-ui/react'
+import { Container, Flex, Skeleton, SkeletonCircle, VStack, Box } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { FeedPost } from './FeedPost'
 
@@ -12,7 +12,24 @@ export const FeedPosts = () => {
 },[]);
   return (
     <Container maxW={"container.sm"} py={10} px={2}>
-        <FeedPost
+      {isLoading && [0,1,2,3].map((_, idx) => (
+        <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10} >
+          <Flex gap={2}>
+            <SkeletonCircle size={10} />
+            <VStack gap={2} alignItems={"flex-start"}>
+              <Skeleton height="10px" w={"200px"}/>
+              <Skeleton height="10px" w={"200px"}/>
+            </VStack>
+          </Flex>
+          <Skeleton w={"full"}>
+            <Box h={"500px"}> contents wrapped</Box>
+          </Skeleton>
+        </VStack>
+        ))}
+
+        {!isLoading && (
+          <>
+          <FeedPost
           username = "coding_locker"
           img = '/img1.png'
           avatar = '/img1.png'
@@ -32,6 +49,9 @@ export const FeedPosts = () => {
           img = '/img4.png'
           avatar = '/img4.png' 
           />
+          </>
+        )}
+
     </Container>
   )
 }
